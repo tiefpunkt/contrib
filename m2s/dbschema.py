6a3cc908f63e04bc8f191102ab16483b5c959844
Jan-Piet Mens <jpmens@gmail.com>
2013-12-05 11:33:55 +0100

    add batt and waypoint 	You don't have to DROP your current db table. 	mysql> alter table location add batt varchar(6); 	mysql> alter table location add waypoint blob;

--- m2s/dbschema.py
+++ m2s/dbschema.py
@@ -24,6 +24,8 @@ class Location(MySQLModel):
     lon             = CharField(null=False)
     tst             = DateTimeField(default=datetime.datetime.now, index=True)
     acc             = CharField(null=True)
+    batt            = CharField(null=True)
+    waypoint        = BlobField(null=True)  # desc in JSON, but desc is reserved SQL word
     # optional: full JSON of item including all data from plugins
     json            = TextField(null=True)
     # the following fields must be correlated to settings.py (plugin columns)
